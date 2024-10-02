1ed4f0ef8a36e4fbfd48389cefbe7b6b02f96791
unknown <sebastian.rivas@uao.edu.co>
2024-10-01 20:29:22 -0500

    Se añadio contenido principal y se soluciono los inconvenientes con la barra de navegación

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { NavLink, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './assets/Componentes/Home'; // Importa el componente Home
+import Projects from './assets/Componentes/Projects'; // Importa el componente Projects
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () =>{
+    setIsMenuOpen(!isMenuOpen)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <nav className='navbar'>
+        <div className='navbar-logo'>MyPortfolio</div>
+        <div className='menu' onClick={toggleMenu}>
+          <div className='menu-icon'></div>
+          <div className='menu-icon'></div>
+          <div className='menu-icon'></div>
+        </div>
+        <ul className={isMenuOpen? 'navbar-links active' : 'navbar-links '}>
+          <li>
+            <NavLink to='/' onClick={toggleMenu}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to='/projects' onClick={toggleMenu}>Projects</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </Router>
   )
 }
 
